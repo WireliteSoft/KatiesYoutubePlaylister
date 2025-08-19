6a6fcd4f57d96666d45bdafa9efd21b1c38d3d78
WireliteSoft <frostic1@yahoo.com>
2025-08-19 16:59:45 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { VideoCollection } from './components/VideoCollection';
 import { PlaylistManager } from './components/PlaylistManager';
 import { VideoPlayer } from './components/VideoPlayer';
 import { useLocalStorage } from './hooks/useLocalStorage';
-import { loadRemote, saveRemote, mirrorToLocalStorage, readMirrorFromLocalStorage, upsertPlaylistMapping } from './utils/snapshot';
+import { loadRemote, saveRemote, mirrorToLocalStorage, readMirrorFromLocalStorage, upsertPlaylistMapping, } from './utils/snapshot';
 
 function App() {
   const [videos, setVideos] = useLocalStorage<Video[]>('videos', []);
